341678ad934131c6f0e687bda9f0ac709e322baf
Yalan Zhang <yalzhang@redhat.com>
2025-12-25 15:34:31 +0800

    update the csv file
    Signed-off-by: Yalan Zhang <yalzhang@redhat.com>

--- operator/compute-pcrs/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: Jakob Naucke <jnaucke@redhat.com>
-//
-// SPDX-License-Identifier: MIT
-
-// This pseudo-library exists to build dependencies in a lower
-// container image layer, which speeds up development.
